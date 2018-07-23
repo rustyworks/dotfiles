fcb0be7debba1103c88b05fd7780a36d52d352be
Tristanto <tristanto.kurniawan@gmail.com>
2018-07-23 10:59:39 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+What todo next:
+
+1. Disable Guest
+2. Change mouse behavior when needed (natural scrolling)
+3. Change touchpad (add three finger moves from accessbility)
+4. Load sshkey from drive
+5. Install iterm2 & change themes
+6. Install postman
+7. Change browser configuration for not tracking bla bla
+8. Install ad block for browser
+9. Change shortcut
---
+++ install.sh
@@ -0,0 +1,31 @@
+# Installing homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew update
+brew list
+# brew install ansible
+brew install ffmpeg graphicsmagick heroku htop imagemagick neovim phantomjs postgresql pyenv rbenv redis ruby-build sqlite the_silver_searcher tree unrar wget youtube-dl
+brew services start postgresql
+brew cask list
+brew tap caskroom/fonts
+brew cask install font-hack-nerd-font
+
+# Installing zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# copy from .zshrc
+source ~/.zshrc
+
+# Installing python and ruby (using pyenv and rbenv)
+# Maybe should manually choose
+pyenv install 3.6.5
+rbenv install 2.5.1
+
+
+# Installing utilities
+mkdir ~/Tools
+mkdir ~/Workspaces
+cd ~/Tools
+git clone git@github.com:rustyworks/vim-config.git
+cd vim-config
+./link.sh
+cd ..
+git clone git@github.com:rustyworks/fonts.git
